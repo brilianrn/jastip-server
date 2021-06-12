2bf78149d93557b5ac855f06bfc3f859723b910f
MacOS <brilian.rachmad13@gmail.com>
2021-06-12 11:48:04 +0700

    update cart for increment or decrement qty

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,4 +1,4 @@
-const { getDatabase } = require('../config');
+const { getDatabase } = require('../config/index');
 const { ObjectId } = require('mongodb');
 const cartCollection = 'carts';
 
@@ -13,6 +13,26 @@ class Cart {
 
   static findByUserId(userId) {
     return getDatabase().collection(cartCollection).find({ UserId: ObjectId(userId) }).toArray();
+  }
+
+  static findOne(cartId) {
+    return getDatabase().collection(cartCollection).find({ _id: ObjectId(cartId) }).toArray();
+  }
+
+  static updateCart({ cartId, dataUpdate }) {
+    return getDatabase().collection(cartCollection).update({ _id: ObjectId(cartId) }, { $set: dataUpdate });
+    // return getDatabase().collection(cartCollection).update({ _id: ObjectId(cartId) }, {
+    //   $set: {
+    //     title: dataUpdate.title,
+    //     size: dataUpdate.size,
+    //     qty: dataUpdate.qty,
+    //     images: dataUpdate.images,
+    //     discExp: dataUpdate.discExp,
+    //     UserId: dataUpdate.UserId,
+    //     ProductId: dataUpdate.ProductId
+    //   }
+    // })
+
   }
 }
 
